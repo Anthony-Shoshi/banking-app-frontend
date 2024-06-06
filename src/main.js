2a0e118ff00b1ee8c988b39e18455ce99a862579
Ador Negash <adordawit4@gmail.com>
2024-06-07 01:33:41 +0200

    xx

--- src/main.js
+++ src/main.js
@@ -1,32 +1,16 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
 import store from './stores/User';
-import axios from 'axios';
 
-// Set the default base URL for Axios
-axios.defaults.baseURL = 'http://localhost:8080';
-const token = localStorage.getItem('token');
-if (token) {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-}
+import './assets/main.css'
 
-// Adding Axios interceptors
-axios.interceptors.request.use(request => {
-    console.log('Starting Request', request);
-    return request;
-});
+const app = createApp(App)
 
-axios.interceptors.response.use(response => {
-    console.log('Response:', response);
-    return response;
-}, error => {
-    // Always handle errors
-    console.error('Response Error:', error);
-    return Promise.reject(error);
-});
-
-const app = createApp(App);
-app.use(router);
 app.use(store);
-app.mount('#app');
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
