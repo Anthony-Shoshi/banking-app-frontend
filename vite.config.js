2269135193064c2e085d2dda039779b654892e87
Muhammad Faizan <701765@student.inholland.nl>
2024-06-11 08:53:04 +0200

    s

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/banking-app-frontend/',
   plugins: [vue()],
   resolve: {
     alias: {
